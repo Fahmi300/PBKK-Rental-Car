9c537b11f1fb67d1b85eb856b9fb80503940d663
jevon <145772135+jevonmanurung@users.noreply.github.com>
2024-12-09 03:52:05 +0700

    Update car.go

--- Backend/routes/car.go
+++ Backend/routes/car.go
@@ -2,8 +2,8 @@ package routes
 
 import (
 	"car-rental-app/controller"
-	"car-rental-app/service"
 	"car-rental-app/middleware"
+	"car-rental-app/service"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,6 +16,7 @@ func CarRoutes(router *gin.Engine, CarController controller.CarController, jwtSe
 		carRoutes.DELETE("/", middleware.Authenticate(jwtService), CarController.DeleteCar)
 		carRoutes.PUT("/", middleware.Authenticate(jwtService), CarController.UpdateCar)
 		carRoutes.GET("/:car_id", CarController.GetCar)
+		carRoutes.GET("/:car_id/image", CarController.GetCarImage)
 
 	}
 }
